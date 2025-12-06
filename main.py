51ee70f6e32b5ac247d36b913e04c21f369b8843
Will Bradford <will@Wills-MacBook-Pro-2.local>
2025-12-06 17:38:20 -0500

    Add command line args for main.py

---
+++ main.py
@@ -0,0 +1,69 @@
+"""
+Generate a dashboard showing
+"""
+from argparse import ArgumentParser, Namespace
+from pathlib import Path
+
+def get_user_args() -> Namespace:
+    """
+    Get and parse command line arguments.
+
+    :return: Description
+    :rtype: Namespace
+    """
+    arg_parser: ArgumentParser = ArgumentParser(
+        description="Collate inputted credit card statements into a single dashboard"
+    )
+
+    arg_parser.add_argument(
+        "-a",
+        "--apple",
+        metavar="APPLE CARD PATH",
+        type=str,
+        default="",
+        help="Path to Apple credit card statement. Must be .csv"
+    )
+
+    arg_parser.add_argument(
+        "-c",
+        "--chase",
+        metavar="CHASE CARD PATH",
+        type=str,
+        default="",
+        help="Path to Chase credit card statement. Must be .csv"
+    )
+
+    arg_parser.add_argument(
+        "-e",
+        "--amex",
+        metavar="AMEX CARD PATH",
+        type=str,
+        default="",
+        help="Path to American Express credit card statement. Must be .csv"
+    )
+
+    return arg_parser.parse_args()
+
+def validate_user_args(apple: Path, chase: Path, amex: Path) -> None:
+
+    # Check Apple path
+    if not apple.is_file():
+        raise # TODO: Implement error (see https://www.geeksforgeeks.org/python/how-to-handle-invalid-arguments-with-argparse-in-python/)
+
+
+if __name__ == "__main__":
+    # Grab command line arguments
+    user_args: Namespace = get_user_args()
+
+    # Translate args to Path objects
+    apple_path: Path = Path(user_args.apple)
+    chase_path: Path = Path(user_args.chase)
+    amex_path:  Path = Path(user_args.amex)
+
+    # Validate args
+    try:
+        validate_user_args(apple_path, chase_path, amex_path)
+
+    # TODO: Specify error type?
+    except Exception as e:
+        print(e)
